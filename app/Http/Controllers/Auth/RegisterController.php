d8098c73b88e67efca20cf7144ce8e9ebb59d7fa
vladlauren <vladlauren99@yahoo.com>
2020-07-29 11:46:31 +0300

    File updated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -89,23 +89,22 @@ class RegisterController extends Controller
 
     public function register(Request $request)
     {
-        //Validate the incoming request using the already included validator method
+        
         $this->validator($request->all())->validate();
 
-        // Initialise the 2FA class
+        
         $google2fa = app('pragmarx.google2fa');
 
-        // Save the registration data in an array
+        
         $registration_data = $request->all();
 
-        // Add the secret key to the registration data
+        
         $registration_data["google2fa_secret"] = $google2fa->generateSecretKey();
 
-        // Save the registration data to the user session for just the next request
+        
         $request->session()->flash('registration_data', $registration_data);
 
-        // Generate the QR image. This is the image the user will scan with their app
-     // to set up two factor authentication
+       
       /* $QR_Image = $google2fa->getQRCodeInLine(
             config('app.name'),
             $registration_data['email'],
@@ -113,17 +112,17 @@ class RegisterController extends Controller
         );
       */
 
-        // Pass the QR barcode image to our view
+      
         return view('google2fa.register', [  'secret' => $registration_data['google2fa_secret']]);
     }
     
 
     public function completeRegistration(Request $request)
     {        
-        // add the session data back to the request input
+       
         $request->merge(session('registration_data'));
 
-        // Call the default laravel authentication
+       
         return $this->registration($request);
     }
   
